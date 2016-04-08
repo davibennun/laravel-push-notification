15caea2aa6b3309051aa370ef4cd51503fab7402
Luis <l.alvarez@gbh.com.do>
2016-04-08 14:37:06 -0400

    correct config file to use in merge request

--- src/config/config.php
+++ src/config/config.php
@@ -6,12 +6,12 @@ return array(
         'environment' =>'development',
         'certificate' =>'/path/to/certificate.pem',
         'passPhrase'  =>'password',
-        'service'     => Sly\NotificationPusher\Adapter\Apns::class
+        'service'     => \Sly\NotificationPusher\Adapter\Apns::class
     ),
     'appNameAndroid' => array(
         'environment' =>'production',
         'apiKey'      =>'yourAPIKey',
-        'service'     => Sly\NotificationPusher\Adapter\Gcm::class
+        'service'     => \Sly\NotificationPusher\Adapter\Gcm::class
     )
 
 );
